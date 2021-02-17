e30f32c4d7d18a51b36cd80186f2e03388d30a6f
Charles Jekal <charles@datasurge.com>
2021-02-16 20:57:29 -0800

    Adding REST controller for hello-worlding

---
+++ src/main/java/com/highlight/helloworld/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.highlight.helloworld;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World!";
+	}
+
+}
