b991af58bb62f2bf9039f5f1723be3a94b9137c8
Charles Jekal <charles@datasurge.com>
2021-02-17 22:23:46 -0800

    disabling CORS and returning JSON

--- src/main/java/com/highlight/helloworld/HelloWorldController.java
+++ src/main/java/com/highlight/helloworld/HelloWorldController.java
@@ -1,14 +1,18 @@
 package com.highlight.helloworld;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloWorldController {
 
+	private final AtomicLong counter = new AtomicLong();
+
 	@RequestMapping("/")
-	public String index() {
-		return "Hello World!";
+	public Greeting index() {
+		return new Greeting(counter.incrementAndGet(), "Hello world!");
 	}
 
 }
--- src/main/java/com/highlight/helloworld/HelloworldApplication.java
+++ src/main/java/com/highlight/helloworld/HelloworldApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class HelloworldApplication {
@@ -27,6 +29,16 @@ public class HelloworldApplication {
 				System.out.println(beanName);
 			}
 
+		};
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
 		};
 	}
 }
